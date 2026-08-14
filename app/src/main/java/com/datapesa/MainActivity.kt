e475ddfeb9c1157fcd1c506fc76e7e516dd67d0b
Isackskyla <oywerinewton@gmail.com>
2026-08-14 20:37:55 +0300

    Initial commit: DataPesa Android app with Hello World

---
+++ app/src/main/java/com/datapesa/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.datapesa.app
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.helloButton)
+        val statusText = findViewById<TextView>(R.id.statusText)
+
+        button.setOnClickListener {
+            statusText.text = "Button clicked! 🎉"
+        }
+    }
+}
